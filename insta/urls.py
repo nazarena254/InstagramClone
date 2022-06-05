419cb4970fa55ded8dcfb8e1e69f6c1c30d1065b
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-05 20:58:54 +0300

    add urlpatterns for my view functions

--- insta/urls.py
+++ insta/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
+from django.urls import path,re_path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.welcome, name='welcome'),
+    path('singup/', views.signup_view, name='signup'),
+    path('comment/<post_id>', views.comment, name='comment'),
+    re_path('like/(?P<post_id>\d+)',views.like_post,name='LikePost'),
+    re_path(r'^search/', views.search_results,name='search_results'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.update_profile, name='user_profile'),
+    path('unfollow/<unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<follow>', views.follow, name='follow'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -1,10 +1,3 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-# def index(request):
-#     return HttpResponse('Hello, world.')
-
 from django.http.response import Http404
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
