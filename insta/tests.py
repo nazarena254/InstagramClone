dbaa5c8746e53088ac5f12b5b8f09a76ce030635
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-05 10:19:52 +0300

    add a test for my Post model

--- insta/tests.py
+++ insta/tests.py
@@ -17,5 +17,25 @@ class ProfileTestClass(TestCase):
         self.profile.save_profile()
         profile_saved = Profile.objects.all()
         self.assertTrue(len(profile_saved) > 0)
+        
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.user=User(username='naz')
+        self.user.save()
+        self.profile=Profile(user=self.user,name='nazarena',bio='Stand tall',profile_pic='default.png')
+        self.post=Post(id=1, likes=10, image='default.png', title='food', description='can be taken as meal or snack',user=self.profile)
+    def tearDown(self):
+        Post.objects.all().delete()
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+    def test_save_post(self):
+        saved_post=Post.objects.all().delete()
+        self.assertTrue((len(saved_post))>0)
+    def test_delete_post(self):
+        self.post.delete_post()
+        deleted_post = Post.objects.all()
+        self.assertTrue(len(deleted_post)==0)        
 
  
